53e22c0e71600efe9747242d33463e2675342bd0
Aik123 <aik.1120ilm@gmail.com>
2022-09-15 10:58:56 +0900

    created migration files users, tokyo, clinics

---
+++ migrations/20220915010731_users.js
@@ -0,0 +1,18 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.createTable("users", (t) => {
+    t.increments("id").primary();
+    t.string("user_name", 16).unique().notNullable();
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.dropTable("users");
+};
