7df100f744561b63a423ba26b6cd750aa8fdb510
Ayumi Watanabe <101871194+ayumiwatanabe3@users.noreply.github.com>
2022-09-21 16:16:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -71,10 +71,13 @@ app.get("/clinics/:id", async (req, res) => {
 
 app.get("/", async (req, res) => {
   try {
-    res.send("heloo!エンドポイント繋げて使ってね
-// GET Tokyo's wards list　-->　/wards
-// GET Clinic's list　--> 　/clinics
-// GET a clinic by id　-->　/clinics/:id");
+    res.json({
+      endpoints: {
+        "GET Tokyo's wards list": "/wards",
+        "GET Clinic's list": "/clinics",
+        "GET a clinic by id": "/clinics/:id",
+      },
+    });
     console.log("Succeeeeeeeeeeeeeeeeeeeeeed");
   } catch (err) {
     console.error(err);
