049654a534c023200910d227ebaf802d5e0b387f
Aik123 <aik.1120ilm@gmail.com>
2022-09-26 10:08:24 +0900

    changed codes for heroku

--- knex.js
+++ knex.js
@@ -13,14 +13,10 @@ require("dotenv").config({
 
 const db = knex({
   client: "pg",
-  connection: {
-    user: process.env.DB_USER || "postgres", // e.g. 'my-user'
-    password: process.env.DB_PASS || "cct1clipon", // e.g. 'my-user-password'
-    database: process.env.DB_NAME || "clipon", // e.g. 'my-database'
-    host:
-      process.env.INSTANCE_UNIX_SOCKET ||
-      "/cloudsql/cliponproject:asia-northeast1:clipon-test2", // e.g. '/cloudsql/project:region:instance'
-  },
+  connection:
+    process.env.DATABASE_URL ||
+    `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@127.0.0.1:5432/clipon`,
+  searchPath: "public",
 });
 
 module.exports = db;
