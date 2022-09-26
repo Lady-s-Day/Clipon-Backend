89470b883d9ae03506e8d605d972f725fa44e1a3
Aik123 <aik.1120ilm@gmail.com>
2022-09-26 09:33:03 +0900

    commented out rollback

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 9000;
   try {
     console.log("Running migrations...");
 
-    await db.migrate.rollback(undefined, true);
+    // await db.migrate.rollback(undefined, true);
     await db.migrate.latest();
     await db.seed.run();
 
